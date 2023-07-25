13856f04f5fc1d5352fa41b2c76c507af30954a3
Tomasz789 <lachowiec_95@wp.pl>
2023-07-25 13:09:24 +0200

    Text library added

---
+++ ConsoleApp.TextLib/TextClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.TextLib
+{
+    public static class TextClass
+    {
+        /// <summary>
+        /// Returns a number of unequal characters in text.
+        /// </summary>
+        /// <param name="text">String representation of text.</param>
+        /// <returns></returns>
+        public static int ReturnUnequalsNumber(string text)
+        {
+            return 0;
+        }
+    }
+}
